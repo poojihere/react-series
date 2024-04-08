bd71f4244511b2d14e2f65b68ef175d35bb3367a
poojihere <vishwanadulapoojitha59743@gmail.com>
2024-04-08 10:48:20 +0530

    local storage

---
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -0,0 +1,27 @@
+import { createContext, useContext } from "react"
+
+
+export const TodoContext = createContext({
+
+    todos:[
+        {
+            id: 1,
+            todo: "Todo Message",
+            completed: false
+        }
+    ],
+
+    addTodo: (todo) => {},
+    updateTodo: (id,todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+
+//hook is nothing but a function
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
