828d9d09c312995b5ca30fc56dd83b33ce54dd6a
poojihere <vishwanadulapoojitha59743@gmail.com>
2024-04-15 15:27:37 +0530

    add services to database and storage

---
+++ reduxtoolkkitCrash/src/components/Todos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { UseSelector, useSelector } from 'react-redux'
+
+function Todos() {
+  return (
+    useSelector((state) => {state})
+  )
+}
+
+export default Todos
